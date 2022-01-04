35e27b567cb97db18504df184e761192c576a3a9
bjb1998 <bjb1998@uw.edu>
2022-01-03 22:53:07 -0800

    why isn't the subfolder here

--- Scripts/Classes/Player.js
+++ Scripts/Classes/Player.js
@@ -2,8 +2,4 @@ class Player extends Entity{
     constructor(name, hp, mp, atk, def, mag, acc, luck, res) {
         super(name, hp, mp, atk, def, mag, acc, luck, res);
     };
-
-    getOlder() {
-        this.age++;
-    };
 };
