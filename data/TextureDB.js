0ebaaaa9d3e5ffadd5dc1074f2603fbe6fb6f70c
bjb1998 <bjb1998@uw.edu>
2022-01-31 13:28:02 -0800

    why isn't github pages working

--- data/TextureDB.js
+++ data/TextureDB.js
@@ -1,10 +1,10 @@
-const selectionSprite = new Texture('assets/selector.png');
-const playerMap = new Texture('assets/arrow.PNG');
-const skyboxSprite = new Texture('assets/deathvalley_panorama.jpg');
-const wallSprite = new Texture('assets/wall_texture.jpg');
-const doorSprite = new Texture('assets/door_texture.jpg');
+const selectionSprite = new Texture('./assets/selector.png');
+const playerMap = new Texture('./assets/arrow.PNG');
+const skyboxSprite = new Texture('./assets/deathvalley_panorama.jpg');
+const wallSprite = new Texture('./assets/wall_texture.jpg');
+const doorSprite = new Texture('./assets/door_texture.jpg');
 
-const DUMMYTexture = new Texture('assets/sprites/enemies/DUMMY.png');
-const PlungerTexture = new Texture('assets/sprites/enemies/Plunger.png');
-const MicrowaveTexture = new Texture('assets/sprites/enemies/Microwave.png');
-const VacuumTexture = new Texture('assets/sprites/enemies/Vacuum.png');
+const DUMMYTexture = new Texture('./assets/sprites/enemies/DUMMY.png');
+const PlungerTexture = new Texture('./assets/sprites/enemies/Plunger.png');
+const MicrowaveTexture = new Texture('./assets/sprites/enemies/Microwave.png');
+const VacuumTexture = new Texture('./assets/sprites/enemies/Vacuum.png');
